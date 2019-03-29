fe7471e68a631d103a200f909650ee73912d5220
Micah Young <myoung@pivotal.io>
2019-03-29 12:47:39 -0400

    Add comments to implementation

--- WCFSelfHost/WCFSelfHost/Program.cs
+++ WCFSelfHost/WCFSelfHost/Program.cs
@@ -33,22 +33,25 @@ namespace SelfHost
     {
         static void Main()
         {
+            // use Steeltoe to parse VCAP_APPLICATION env variables into config object
             var builder = new ConfigurationBuilder().AddCloudFoundry();
             var config = builder.Build();
             var opts = new CloudFoundryApplicationOptions();
             var appSection = config.GetSection(CloudFoundryApplicationOptions.CONFIGURATION_PREFIX);
             appSection.Bind(opts);
 
+            // get external TCP route (format: ["fullyqualifieddomainname.com:80000"])
             var appRouteHostAndExternalPort = opts.ApplicationUris.FirstOrDefault().Split(':');
             var appRouteHost = appRouteHostAndExternalPort.ElementAtOrDefault(0);
             var appExternalPort = appRouteHostAndExternalPort.ElementAtOrDefault(1);
-            var appInternalPort = opts.Port.ToString();
-
+          
             if (appRouteHost == "" || appExternalPort == "")
             {
                 throw new System.ArgumentException("Invalid VCAP_APPLICATION route or port");
             }
 
+            // ensure external TCP port and internal listening $PORT are the same
+            var appInternalPort = opts.Port.ToString();
             if (appInternalPort != appExternalPort)
             {
                 throw new System.ArgumentException($"Internal listening port must match External Route port : {appInternalPort} != {appExternalPort}");
@@ -56,16 +59,18 @@ namespace SelfHost
             Console.WriteLine($"URI: {appRouteHost}:{appInternalPort}");
 
 
+            // have endpoints listen on public URI
             var baseAddress = new Uri($"net.tcp://{appRouteHost}:{appInternalPort}/example/service");
-
             var svcHost = new ServiceHost(typeof(HelloWorld), baseAddress);
 
+            // enable verbose errors
             ServiceDebugBehavior debug = svcHost.Description.Behaviors.Find<ServiceDebugBehavior>();
             debug.IncludeExceptionDetailInFaults = true;
 
             var netTcpBinding = new NetTcpBinding();
             netTcpBinding.Security.Mode = SecurityMode.None;
 
+            // use custom binding to reduce connection pool settings to work better in load-balanced scenario (https://stackoverflow.com/questions/9714426/disable-connection-pooling-for-wcf-net-tcp-bindings)
             BindingElementCollection bindingElementCollection = netTcpBinding.CreateBindingElements();
             TcpTransportBindingElement transport = bindingElementCollection.Find<TcpTransportBindingElement>();
             transport.ConnectionPoolSettings.IdleTimeout = TimeSpan.Zero;
@@ -77,6 +82,7 @@ namespace SelfHost
             balancedTcpBinding.Name = "NetTcpBinding";
 
 
+            // add metadata endpoint
             ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
             svcHost.Description.Behaviors.Add(smb);
 
@@ -86,6 +92,7 @@ namespace SelfHost
                 "mex"
             );
 
+            // add service endpoint
             svcHost.AddServiceEndpoint(
                 typeof(IHelloWorld),
                 balancedTcpBinding,
